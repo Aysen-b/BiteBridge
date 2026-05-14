65fc729fa5e9e5a79c1433bbf8eae27c1a3eb9c9
Ayşen Baştürk <basturkaysen@gmail.com>
2026-05-14 14:45:52 +0300

    Add database generation script

---
+++ BiteBridge/database_generation_script.sql
@@ -0,0 +1,216 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [AspNetRoles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [AspNetUsers] (
+    [Id] nvarchar(450) NOT NULL,
+    [RoleName] nvarchar(max) NOT NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [ProviderKey] nvarchar(450) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [Name] nvarchar(450) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260424133809_InitialCreate', N'10.0.7');
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Orders] (
+    [Id] int NOT NULL IDENTITY,
+    [UserEmail] nvarchar(max) NOT NULL,
+    [Items] nvarchar(max) NOT NULL,
+    [TotalPrice] int NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260425144634_AddOrderTable', N'10.0.7');
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [SystemLogs] (
+    [Id] int NOT NULL IDENTITY,
+    [CreatedAt] datetime2 NOT NULL,
+    [Level] nvarchar(max) NOT NULL,
+    [Action] nvarchar(max) NOT NULL,
+    [UserEmail] nvarchar(max) NULL,
+    [Details] nvarchar(max) NULL,
+    CONSTRAINT [PK_SystemLogs] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260514074721_AddSystemLogs', N'10.0.7');
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Caterers] (
+    [Id] int NOT NULL IDENTITY,
+    [BusinessName] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [Address] nvarchar(max) NULL,
+    [OwnerEmail] nvarchar(max) NULL,
+    [Latitude] float NOT NULL,
+    [Longitude] float NOT NULL,
+    CONSTRAINT [PK_Caterers] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [MenuItems] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [Price] decimal(18,2) NOT NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    [CatererId] int NOT NULL,
+    CONSTRAINT [PK_MenuItems] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_MenuItems_Caterers_CatererId] FOREIGN KEY ([CatererId]) REFERENCES [Caterers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [MenuItemOptions] (
+    [Id] int NOT NULL IDENTITY,
+    [OptionName] nvarchar(max) NOT NULL,
+    [OptionType] nvarchar(max) NOT NULL,
+    [ExtraPrice] decimal(18,2) NOT NULL,
+    [MenuItemId] int NOT NULL,
+    CONSTRAINT [PK_MenuItemOptions] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_MenuItemOptions_MenuItems_MenuItemId] FOREIGN KEY ([MenuItemId]) REFERENCES [MenuItems] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_MenuItemOptions_MenuItemId] ON [MenuItemOptions] ([MenuItemId]);
+
+CREATE INDEX [IX_MenuItems_CatererId] ON [MenuItems] ([CatererId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260514082430_AddMenuSystemTables', N'10.0.7');
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Ratings] (
+    [Id] int NOT NULL IDENTITY,
+    [OrderId] int NOT NULL,
+    [UserEmail] nvarchar(max) NOT NULL,
+    [Score] int NOT NULL,
+    [Comment] nvarchar(max) NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    CONSTRAINT [PK_Ratings] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Ratings_Orders_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [Orders] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Ratings_OrderId] ON [Ratings] ([OrderId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260514101527_AddRatings', N'10.0.7');
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [EmailNotifications] (
+    [Id] int NOT NULL IDENTITY,
+    [UserEmail] nvarchar(max) NOT NULL,
+    [Subject] nvarchar(max) NOT NULL,
+    [Message] nvarchar(max) NOT NULL,
+    [OrderId] int NULL,
+    [IsRead] bit NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    CONSTRAINT [PK_EmailNotifications] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260514102856_AddEmailNotifications', N'10.0.7');
+
+COMMIT;
+GO
+
